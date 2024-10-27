8ddc4dda9fe4e9636c1d7ea0470d0b5295aa4d4d
lfhwnqe <lfhwnqea@gmail.com>
2024-10-27 20:41:45 +0800

    feature: 修改构建配置

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  images: {
+    unoptimized: true, // 如果使用了 Next.js Image 组件
+  },
 };
 
 export default nextConfig;
