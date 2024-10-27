475ab9ba1ca1b2c44a0ca3c3a33ec24708c1b0ca
lfhwnqe <lfhwnqea@gmail.com>
2024-10-27 21:10:34 +0800

    fix: 修改构建脚本

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  output: 'export',  // 改成 export 因为日志显示在生成静态页面
   images: {
     unoptimized: true, // 如果使用了 Next.js Image 组件
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
